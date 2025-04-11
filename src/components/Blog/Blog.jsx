343c8e872596a34fb11ce9c6d727ad8453bc23a2
gmnayem631 <you@example.comgmnayem631@gmail.com>
2025-04-11 06:49:02 +0600

    added author details, hashtags, and bookmark button

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegBookmark } from "react-icons/fa";
 
 const Blog = ({ blog }) => {
   //   const { blog } = props;
@@ -11,11 +12,21 @@ const Blog = ({ blog }) => {
           <img src={blog.cover} alt="Shoes" />
         </figure>
         <div className="card-body">
+          <div className="author flex justify-around items-center">
+            <h3>{blog.author}</h3>
+            <img className="w-16" src={blog.author_img} alt="" />
+            <FaRegBookmark size={25} />
+          </div>
           <h2 className="card-title">{blog.title}</h2>
           <p>
             A card component has a figure, a body part, and inside body there
             are title and actions parts
           </p>
+          <div className="flex">
+            {blog.hashtags.map((hash) => (
+              <p>{hash}</p>
+            ))}
+          </div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Mark as Read</button>
           </div>
